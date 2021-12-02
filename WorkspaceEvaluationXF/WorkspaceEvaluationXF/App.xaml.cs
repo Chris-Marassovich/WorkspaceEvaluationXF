fb14ba61a29ed10a97bdb3833cdeeb734ad1d24c
Chris Marrasovich <chris.marassovich@distillerysoftware.com>
2021-12-02 11:28:45 +1100

    Add project files.

---
+++ WorkspaceEvaluationXF/WorkspaceEvaluationXF/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using WorkspaceEvaluationXF.Services;
+using WorkspaceEvaluationXF.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WorkspaceEvaluationXF
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ WorkspaceEvaluationXF/WorkspaceEvaluationXF/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using WorkspaceEvaluationXF.ViewModels;
+using Xamarin.Forms;
+
+namespace WorkspaceEvaluationXF.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
